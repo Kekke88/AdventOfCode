b89879a946210f515ee5bd9936183dfe0df6e5b6
HenricJohansson <henric-johansson@hotmail.com>
2021-12-01 15:24:24 +0100

    2021: added execution time

--- 2021/tasks/01/app.php
+++ 2021/tasks/01/app.php
@@ -15,40 +15,6 @@ $parser = new Parser(dirname(__FILE__) . '/data.in');
 $measurements = $parser->read(new MeasurementReader);
 $increases = 0;
 
-function array_windows($arr, $num)
-{
-	$retArr = [];
-
-	for ($i = 0; $i < sizeof($arr) - ($num - 1); $i++) {
-		$retArr[] = [$arr[$i], $arr[$i + 1], $arr[$i + 2]];
-	}
-
-	return $retArr;
-}
-
-$i = $j = 0;
-$prev = PHP_INT_MAX;
-foreach (array_windows($measurements, 3) as $measurement) {
-	if ($measurement[0] < $measurement[1] || ($i == 0 && $measurement[1] < $measurement[2])) {
-		$i++;
-	}
-
-	$val = $measurement[0]->value + $measurement[1]->value + $measurement[2]->value;
-	if ($prev < $val) {
-		$j++;
-	}
-	$prev = $val;
-}
-
-$timeEnd = microtime(true);
-$executionTime = ($timeEnd - $timeStart) * 1000;
-
-echo "Part 1: Number of increases is {$i}." . PHP_EOL;
-echo "Part 2: Number of increases is {$j}." . PHP_EOL . PHP_EOL;
-echo "Execution time (ms): {$executionTime}" . PHP_EOL;
-
-die();
-
 for ($i = 1; $i < sizeof($measurements); $i++) {
 	$increases = $measurements[$i] > $measurements[$i - 1] ? $increases + 1 : $increases;
 }
