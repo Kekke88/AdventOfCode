12b098e8d257cf4ffe2ef285988f23d11a63d7bf
HenricJohansson <henric-johansson@hotmail.com>
2021-12-13 18:01:20 +0100

    2021: day 13 fixet output

--- 2021/tasks/13/app.php
+++ 2021/tasks/13/app.php
@@ -9,16 +9,17 @@ $timeStart = microtime(true);
 $parser = new Parser(dirname(__FILE__) . '/data.in');
 
 $manual = $parser->read(new ThermalManualReader);
-//$manual->debug();
-$manual->fold();
-echo PHP_EOL;
-$manual->debug();
+
+$manual->fold(1);
 $dots = $manual->countDots();
+
+$manual->fold();
+$dots2 = $manual->countDots();
 
 $timeEnd = microtime(true);
 $executionTime = ($timeEnd - $timeStart) * 1000;
 
 echo "Part 1: Dots={$dots}" . PHP_EOL;
-//echo "Part 2: Paths={$numberOfAdvancedPaths}" . PHP_EOL;
+echo "Part 2: Dots={$dots2}" . PHP_EOL;
 echo "Execution time (ms): {$executionTime}" . PHP_EOL;
-// Execution time (ms): 904.52790260315
+// Execution time (ms): 19.383907318115
